0d866c60b85bd1f7a62b01c51a03e446dadc1096
Noitcereon <tba@live.dk>
2023-02-12 16:23:00 +0100

    Add empty ElOverblikApiController

---
+++ src/main/java/me/noitcereon/external/api/eloverblik/ElOverblikApiController.java
@@ -0,0 +1,13 @@
+package me.noitcereon.external.api.eloverblik;
+
+
+/**
+ * Handles all calls to the "El Overblik" API.
+ * @see <a href="https://api.eloverblik.dk/CustomerApi/index.html">El Overblik API Swagger</a>
+ * @author Noitcereon
+ * @since 0.0.1
+ */
+public class ElOverblikApiController {
+    // TODO: look into how Java can call API via HttpClient
+    // TODO: Call El Overblik to get electricity data for a given household.
+}

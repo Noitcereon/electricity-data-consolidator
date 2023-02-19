da778a157ce69cbe1c2e0a57f7ffbd46cda15563
Noitcereon <tba@live.dk>
2023-02-19 12:39:19 +0100

    Feat: Add constructor to NotImplementedException

--- src/main/java/me/noitcereon/exceptions/NotImplementedException.java
+++ src/main/java/me/noitcereon/exceptions/NotImplementedException.java
@@ -4,4 +4,7 @@ public class NotImplementedException extends RuntimeException{
     public NotImplementedException(){
         super("This method is not implemented yet!");
     }
+    public NotImplementedException(String message){
+        super(message);
+    }
 }

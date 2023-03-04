76e7c2822e8d62c6d50d4c7173249cbaa286ff35
Noitcereon <tba@live.dk>
2023-03-04 23:49:42 +0100

    Feat: Add ElectricityConsolidatorRuntimeException

---
+++ src/main/java/me/noitcereon/exceptions/ElectricityConsolidatorRuntimeException.java
@@ -0,0 +1,9 @@
+package me.noitcereon.exceptions;
+
+/**
+ * A custom general purpose RuntimeException.
+ * Meant to help differentiate between a regular RuntimeException and one made by the application.
+ */
+public class ElectricityConsolidatorRuntimeException extends RuntimeException {
+
+}

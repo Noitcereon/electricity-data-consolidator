21abbd6b898393bfed33027ce492a1e7409082fe
Noitcereon <tba@live.dk>
2023-03-12 12:38:39 +0100

    Feat: Add ConfigurationKeys

---
+++ src/main/java/me/noitcereon/configuration/ConfigurationKeys.java
@@ -0,0 +1,12 @@
+package me.noitcereon.configuration;
+
+/**
+ * To prevent typos when retrieving configuration properties this class holds a static final reference you can use.
+ */
+public class ConfigurationKeys {
+    private ConfigurationKeys() {
+        // Prevent instantiation.
+    }
+    public static final String API_KEY = "api-key";
+    public static final String DATA_ACCESS_TOKEN = "data-access-token";
+}

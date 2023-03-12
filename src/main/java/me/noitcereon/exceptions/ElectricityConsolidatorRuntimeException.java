8d7debb192777d47119f6993e0b1f8466d1ab538
Noitcereon <tba@live.dk>
2023-03-12 10:47:19 +0100

    Feat: Add constructors to ElectricityConsolidatorRuntimeException.java

--- src/main/java/me/noitcereon/exceptions/ElectricityConsolidatorRuntimeException.java
+++ src/main/java/me/noitcereon/exceptions/ElectricityConsolidatorRuntimeException.java
@@ -6,4 +6,22 @@ package me.noitcereon.exceptions;
  */
 public class ElectricityConsolidatorRuntimeException extends RuntimeException {
 
+    public ElectricityConsolidatorRuntimeException(){
+        super();
+    }
+    public ElectricityConsolidatorRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    public ElectricityConsolidatorRuntimeException(String message) {
+        super(message);
+    }
+
+    public ElectricityConsolidatorRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ElectricityConsolidatorRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }

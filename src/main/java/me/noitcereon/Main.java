b47cbfb021fcaaa82f41ef4e0eaef89f5f7839c3
Noitcereon <tba@live.dk>
2023-05-19 21:24:22 +0200

    Feat: Add work-in-progress message on startup

--- src/main/java/me/noitcereon/Main.java
+++ src/main/java/me/noitcereon/Main.java
@@ -1,11 +1,17 @@
 package me.noitcereon;
 
-import me.noitcereon.configuration.ConfigLoader;
+
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        ConfigLoader configLoader = ConfigLoader.getInstance();
-        String apiKey = configLoader.getProperty("eloverblik-api-key");
-        System.out.println(apiKey);
+    public static void main(String[] args) throws IOException {
+        System.out.println("Welcome to this unfinished console application.");
+        System.out.println("The intention is to make a simple application, which can ");
+        System.out.println("retrieve data from various electricty data APIs.");
+        System.out.println("For now everything is being tested via JUnit tests.");
+        System.out.println("You may want to take a look at 'ElOverblikApiController' to start with :)");
+        System.out.println("Press ENTER to exit...");
+        System.in.read(); // intentionally ignored.
+        System.out.println("Exiting...");
     }
 }

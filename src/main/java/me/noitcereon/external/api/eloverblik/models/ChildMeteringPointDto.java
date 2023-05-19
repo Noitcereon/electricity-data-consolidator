19a59d043c512a7de108c8bca71865d311c56860
Noitcereon <tba@live.dk>
2023-05-19 20:06:25 +0200

    Feat: Add MeteringPointApiDtoListApiResponse and related POJOs
    POJO = Plain Old Java Object

---
+++ src/main/java/me/noitcereon/external/api/eloverblik/models/ChildMeteringPointDto.java
@@ -0,0 +1,66 @@
+package me.noitcereon.external.api.eloverblik.models;
+
+public class ChildMeteringPointDto {
+    private String parentMeteringPointId;
+    private String meteringPointId;
+    private String typeOfMP;
+    private String meterReadingOccurrence;
+    private String meterNumber;
+
+
+    public ChildMeteringPointDto() {
+        // Empty contructor to allow serialization
+    }
+
+    public String getParentMeteringPointId() {
+        return parentMeteringPointId;
+    }
+
+    public void setParentMeteringPointId(String parentMeteringPointId) {
+        this.parentMeteringPointId = parentMeteringPointId;
+    }
+
+    public String getMeteringPointId() {
+        return meteringPointId;
+    }
+
+    public void setMeteringPointId(String meteringPointId) {
+        this.meteringPointId = meteringPointId;
+    }
+
+    public String getTypeOfMP() {
+        return typeOfMP;
+    }
+
+    public void setTypeOfMP(String typeOfMP) {
+        this.typeOfMP = typeOfMP;
+    }
+
+    public String getMeterReadingOccurrence() {
+        return meterReadingOccurrence;
+    }
+
+    public void setMeterReadingOccurrence(String meterReadingOccurrence) {
+        this.meterReadingOccurrence = meterReadingOccurrence;
+    }
+
+    public String getMeterNumber() {
+        return meterNumber;
+    }
+
+    public void setMeterNumber(String meterNumber) {
+        this.meterNumber = meterNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "ChildMeteringPointDto{" +
+                "parentMeteringPointId='" + parentMeteringPointId + '\'' +
+                ", meteringPointId='" + meteringPointId + '\'' +
+                ", typeOfMP='" + typeOfMP + '\'' +
+                ", meterReadingOccurrence='" + meterReadingOccurrence + '\'' +
+                ", meterNumber='" + meterNumber + '\'' +
+                '}';
+    }
+
+}

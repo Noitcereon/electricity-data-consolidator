537ff577292cc911991c486d55d0cf390159127d
Noitcereon <tba@live.dk>
2023-05-20 13:37:10 +0200

    Feat: Overhaul ElOverblikApiController
    This was to:
    1. Use the new DataAccessTokenManager
    2. Use Configuration interfaces instead of direct config classes
    3. Add a way to LOG

--- src/main/java/me/noitcereon/external/api/eloverblik/ElOverblikApiController.java
+++ src/main/java/me/noitcereon/external/api/eloverblik/ElOverblikApiController.java
@@ -1,14 +1,10 @@
 package me.noitcereon.external.api.eloverblik;
 
 
-import me.noitcereon.configuration.ConfigLoader;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import me.noitcereon.configuration.*;
+import me.noitcereon.external.api.eloverblik.data.access.DataAccessTokenManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Handles all calls to the "El Overblik" API.
@@ -20,28 +16,30 @@ import java.net.http.HttpResponse;
 public class ElOverblikApiController {
     // TODO: look into how Java can call API via HttpClient
     // TODO: Call El Overblik to get electricity data for a given household.
-    private ConfigLoader configLoader = ConfigLoader.getInstance();
+    private final DataAccessTokenManager dataAccessTokenManager;
+    private static final Logger LOG = LoggerFactory.getLogger(ElOverblikApiController.class);
+    private final ConfigurationLoader configLoader;
+    private final ConfigurationSaver configSaver;
 
-    public void retrieveDataAccessToken() {
-        try {
-            // TODO: refactor this method
-            // TODO: restrict access to this method and cache the data access key for the remainder of the day.
-            URI getDataAccessTokenEndPoint = new URI("https://api.eloverblik.dk/customerapi/api/token");
-            HttpClient httpClient = HttpClient.newHttpClient();
+    public ElOverblikApiController() {
+        configSaver = SimpleConfigSaver.getInstance();
+        configLoader = SimpleConfigLoader.getInstance();
+        dataAccessTokenManager = new DataAccessTokenManager();
+    }
+    public ElOverblikApiController(ConfigurationSaver configSaver, ConfigurationLoader configLoader) {
+        this.configSaver = configSaver;
+        this.configLoader = configLoader;
+        dataAccessTokenManager = new DataAccessTokenManager(configSaver, configLoader);
+    }
 
-            HttpRequest httpRequest =
-                    HttpRequest.newBuilder(getDataAccessTokenEndPoint)
-                    .header("Authorization", "Bearer " + configLoader.getProperty("eloverblik-api-key"))
-                    .build();
+    // Constructor intended for testing (make everything mockable)
+    public ElOverblikApiController(ConfigurationSaver configSaver, ConfigurationLoader configLoader, DataAccessTokenManager dataAccessTokenManager) {
+        this.configSaver = configSaver;
+        this.configLoader = configLoader;
+        this.dataAccessTokenManager = dataAccessTokenManager;
+    }
 
-            HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-            System.out.println(response);
-        } catch (URISyntaxException e) {
-            System.err.println("Invalid URI");
-        } catch (IOException e) {
-            System.err.println(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    public String retrieveDataAccessToken(){
+        return dataAccessTokenManager.retrieveDataAccessToken();
     }
 }

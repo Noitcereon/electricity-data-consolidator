af2cd7a4dd0d8598cbd2a724e562db8e830f04df
Noitcereon <tba@live.dk>
2023-05-20 18:19:06 +0200

    Feat(ElOverblikApi): Add a way to retrieve meteringPoints

--- src/main/java/me/noitcereon/external/api/eloverblik/ElOverblikApiController.java
+++ src/main/java/me/noitcereon/external/api/eloverblik/ElOverblikApiController.java
@@ -2,9 +2,15 @@ package me.noitcereon.external.api.eloverblik;
 
 
 import me.noitcereon.configuration.*;
+import me.noitcereon.exceptions.ElectricityConsolidatorRuntimeException;
 import me.noitcereon.external.api.eloverblik.data.access.DataAccessTokenManager;
+import me.noitcereon.external.api.eloverblik.data.access.MeteringPointManager;
+import me.noitcereon.external.api.eloverblik.models.MeteringPointApiDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Handles all calls to the "El Overblik" API.
@@ -14,9 +20,8 @@ import org.slf4j.LoggerFactory;
  * @since 0.0.1
  */
 public class ElOverblikApiController {
-    // TODO: look into how Java can call API via HttpClient
-    // TODO: Call El Overblik to get electricity data for a given household.
     private final DataAccessTokenManager dataAccessTokenManager;
+    private final MeteringPointManager meteringPointManager;
     private static final Logger LOG = LoggerFactory.getLogger(ElOverblikApiController.class);
     private final ConfigurationLoader configLoader;
     private final ConfigurationSaver configSaver;
@@ -25,21 +30,35 @@ public class ElOverblikApiController {
         configSaver = SimpleConfigSaver.getInstance();
         configLoader = SimpleConfigLoader.getInstance();
         dataAccessTokenManager = new DataAccessTokenManager();
+        meteringPointManager = new MeteringPointManager();
     }
     public ElOverblikApiController(ConfigurationSaver configSaver, ConfigurationLoader configLoader) {
         this.configSaver = configSaver;
         this.configLoader = configLoader;
         dataAccessTokenManager = new DataAccessTokenManager(configSaver, configLoader);
+        meteringPointManager = new MeteringPointManager();
     }
 
     // Constructor intended for testing (make everything mockable)
-    public ElOverblikApiController(ConfigurationSaver configSaver, ConfigurationLoader configLoader, DataAccessTokenManager dataAccessTokenManager) {
+    public ElOverblikApiController(ConfigurationSaver configSaver, ConfigurationLoader configLoader, DataAccessTokenManager dataAccessTokenManager, MeteringPointManager meteringPointManager) {
         this.configSaver = configSaver;
         this.configLoader = configLoader;
         this.dataAccessTokenManager = dataAccessTokenManager;
+        this.meteringPointManager = meteringPointManager;
     }
 
     public String retrieveDataAccessToken(){
         return dataAccessTokenManager.retrieveDataAccessToken();
+    }
+    public List<MeteringPointApiDto> getMeteringPoints(boolean includeAll){
+        try {
+            return meteringPointManager.getMeteringPoints(includeAll);
+        } catch (IOException e) {
+            throw new ElectricityConsolidatorRuntimeException(e);
+        } catch (InterruptedException e) {
+            LOG.warn("Call to getMeteringPoints was interrupted", e);
+            Thread.currentThread().interrupt();
+        }
+        throw new ElectricityConsolidatorRuntimeException("Something went wrong during call to getMeteringPoints. Args: includeAll=" + includeAll);
     }
 }

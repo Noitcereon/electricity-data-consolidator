560a1bd05be84a3153b07e2eecfd0c3b5c68e8ae
Noitcereon <tba@live.dk>
2024-05-11 21:32:18 +0200

    Feat(config): Add option to load config with a fallback value

--- src/main/java/me/noitcereon/configuration/ConfigurationLoader.java
+++ src/main/java/me/noitcereon/configuration/ConfigurationLoader.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 public interface ConfigurationLoader {
     Optional<String> getProperty(String key);
+    Optional<String> getProperty(String key, String fallbackValue);
 
     /**
      * Fetches the latest configuration properties and caches them.
